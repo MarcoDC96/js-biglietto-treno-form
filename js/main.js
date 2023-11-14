56275560b557525e247fef70aaf239fce5d2bab6
MarcoDC96 <marco.decicco@live.it>
2023-11-14 17:34:24 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -3,23 +3,30 @@ function userHasFinishDataCompletion(event){
     event.preventDefault();
 
     let nameSurname = document.getElementById("nameSurname").value;
+    document.getElementById("user_name").innerHTML = nameSurname;
     let kmTot = document.getElementById("kmTot").value;
+    document.getElementById("km_selezionati").innerHTML = kmTot;
     let age = document.getElementById("age").value;
+    document.getElementById("offerta").innerHTML = age;
+
     age = parseInt(age);
     kmTot=parseFloat(kmTot);
     let prezzo = (kmTot*0.21);
 
     if(age=="minorenne"){
-       let prezzoMinorenni = (prezzo*0.8);
+       let prezzoMinorenni = prezzo*0.8;
+       document.getElementById("prezzoFinale").innerHTML = prezzoMinorenni.toFixed(2);
        console.log(prezzoMinorenni.toFixed(2));
     }
 
     else if(age=="over65"){
-        let prezzoOver65 = (prezzo*0.6);
+        let prezzoOver65 = prezzo*0.6;
+        document.getElementById("prezzoFinale").innerHTML = prezzoOver65.toFixed(2);
         console.log(prezzoOver65.toFixed(2));
     }
 
     else{
+        document.getElementById("prezzoFinale").innerHTML = prezzo.toFixed(2);
         console.log(prezzo);
     }
 
